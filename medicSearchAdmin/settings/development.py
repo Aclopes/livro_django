23d49ff206278d0af4ad61b62c6fce2059806d86
Alex Lopes <aclopes@gmail.com>
2022-09-22 16:04:05 -0400

    last configurations

--- medicSearchAdmin/settings/development.py
+++ medicSearchAdmin/settings/development.py
@@ -3,8 +3,9 @@ from .settings import *
 DEBUG = True
 
 # Secret Key
-SECRET_KEY = 'gy4lynBP22W0HwnX"avCz4+5I-MK`CG{[.!Gnm4wO~k4;lDx9'
+SECRET_KEY = 'gy4lynBP22W0HwnX"avCz4+5I-MK`CG{[.!Gnm4wO~k4;lDx9;'
 
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -12,4 +13,6 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.1.*']
+# ALLOWED_HOSTS = ['*'] everything, not safe
+# https://docs.djangoproject.com/en/3.2/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.149']
--- medicSearchAdmin/settings/settings.py
+++ medicSearchAdmin/settings/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1_uz&w@t=ykg^$v1=$3au+p&@o=er9coljb-hcl7h4xy!ue($p'
+# SECRET_KEY = 'django-insecure-1_uz&w@t=ykg^$v1=$3au+p&@o=er9coljb-hcl7h4xy!ue($p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
